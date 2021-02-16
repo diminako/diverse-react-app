d725fc3cbe0666e051cb5c0f2f37a8e61ec119ab
DimitriNakos <dimi1nakos@gmail.com>
2021-02-16 16:01:43 -0500

    time to start making components

--- src/pages/Day001/index.jsx
+++ src/pages/Day001/index.jsx
@@ -1,9 +1,16 @@
+import "./style.css";
 
 
 const Day001 = (props) => {
     return (
-        <div>
-            
+        <div className="main-div">
+            <div className="sabin"></div>
+            <div className="cyan"></div>
+            <div className="train"></div>
+            <div className="menu">
+                <div className="char-move"></div>
+                <div className="enemy"></div>
+            </div>
         </div>
     )
 }
